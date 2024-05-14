9b4fd23a1cd077db66ada0773f7e58cccca06a53
Paul Hendrik Tiemann <paul.hendrik.tiemann@uni-oldenburg.de>
2024-05-14 13:58:58 +0200

    Create folder structure and empty files.

---
+++ hackathon_backend/interface.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@router.get("/market/price")
+async def read_price():
+    return {
+        "data": [
+            {"price": price, "time": i}
+            for i, price in enumerate(market._clearing_prices)
+        ]
+    }
+
+
+@router.put("/market/bid/")
+async def send_bid(bid: Bid):
+    market._bids.append(bid)
+
+
+@router.get("/market/clearing/")
+async def next_clearing():
+    return market._clearing_interval - (time.time() - market._last_clearing)
+
+
+@router.get("/market/bids")
+async def read_bids():
+    aid_to_bids = {}
+    for bid in market._bids:
+        if not bid.aid in aid_to_bids:
+            aid_to_bids[bid.aid] = []
+        aid_to_bids[bid.aid].append(bid)
+
+    return aid_to_bids
--- hackathon_backend/main.py
+++ hackathon_backend/main.py
@@ -2,14 +2,14 @@ import asyncio
 import random
 import time
 from contextlib import asynccontextmanager
+import hackathon_backend.interface as interface
 
-from fastapi import FastAPI
 import logging
 from pydantic import BaseModel
 
 import uuid
 
-from .market import Market, VirtualBiddingAgent, Bid
+from .market.market import Market, VirtualBiddingAgent, Bid
 
 logger = logging.getLogger(__name__)
 
@@ -33,39 +33,4 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/market/price")
-async def read_price():
-    return {
-        "data": [
-            {"price": price, "time": i}
-            for i, price in enumerate(market._clearing_prices)
-        ]
-    }
-
-
-@app.put("/market/bid/")
-async def send_bid(bid: Bid):
-    market._bids.append(bid)
-
-
-@app.get("/market/clearing/")
-async def next_clearing():
-    return market._clearing_interval - (time.time() - market._last_clearing)
-
-
-@app.get("/market/bids")
-async def read_bids():
-    aid_to_bids = {}
-    for bid in market._bids:
-        if not bid.aid in aid_to_bids:
-            aid_to_bids[bid.aid] = []
-        aid_to_bids[bid.aid].append(bid)
-
-    return aid_to_bids
+app.include_router(interface.router)
