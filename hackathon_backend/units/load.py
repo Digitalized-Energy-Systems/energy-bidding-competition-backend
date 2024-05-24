ec6a006724d96752caafd8a32e39c1963c8cd4a6
Paul Hendrik Tiemann <paul.hendrik.tiemann@uni-oldenburg.de>
2024-05-24 10:01:09 +0200

    Provide load forecast in DemandInformation.

--- hackathon_backend/units/load.py
+++ hackathon_backend/units/load.py
@@ -32,17 +32,30 @@ class SimpleDemandUnit(Unit):
         super().__init__(id)
 
         self._simple_demand = simple_demand
+        self.forecast_horizon = 4
+        self.time_step = 0 #None
 
     def step(
         self, input: UnitInput, step: int, other_inputs: Dict[str, UnitInput] = None
     ):
+        print(f'Step Load {self.id} with input {input} and step {step}')
+        self.time_step = step
         p, q = self._simple_demand.forecast_demand(step)
         return UnitResult(p_kw=p, q_kvar=q)
 
     def read_information(self) -> DemandInformation:
-        p, q = self._simple_demand.forecast_demand()
+        p, q = self.get_forecast(start_index=self.time_step+1, end_index=self.time_step+1+self.forecast_horizon)
         return DemandInformation(self.id, p, q)
 
+    def get_forecast(self, start_index, end_index):
+        # TODO specify time frame of forecast
+        p_forecast = []
+        q_forecast = []
+        for i in range(start_index, end_index):
+            p_fcast, q_fcast = self._simple_demand.forecast_demand(i)
+            p_forecast.append(p_fcast)
+            q_forecast.append(q_fcast)
+        return p_forecast, q_forecast
 
 def create_demand(id, p_profile: List, q_profile: List, uncertainty: float):
     # TODO Default Profile
