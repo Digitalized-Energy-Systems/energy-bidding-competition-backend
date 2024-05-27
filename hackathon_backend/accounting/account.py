55e1efb995f7bbcec476df70fb719e5222b13600
Paul Hendrik Tiemann <paul.hendrik.tiemann@uni-oldenburg.de>
2024-05-27 10:36:51 +0200

    Introduce global variable labels.

--- hackathon_backend/accounting/account.py
+++ hackathon_backend/accounting/account.py
@@ -1,24 +1,28 @@
 import pandas as pd
 
+AWARDED_AMOUNT = "Awarded amount in kW"
+PROVIDED_POWER = "Provided power in kW"
+PAYOFF = "Payoff in ct"
+BALANCE = "Balance after transaction in ct"
+
 class Account:
     def __init__(self):
-        self.balance = 0
-        self.transactions = pd.DataFrame(columns=["Awarded amount in kW", "Provided power in kW", "Payoff in ct", "Balance after transaction in ct"])
+        self.transactions = pd.DataFrame(columns=[AWARDED_AMOUNT, PROVIDED_POWER, PAYOFF, BALANCE])
 
     def add_transaction(self, awarded_amount, provided_power, payoff):
-        self.balance += payoff
+        balance = self.transactions[BALANCE].iloc[-1] + payoff
         self.transactions = pd.concat([
             self.transactions,
             pd.DataFrame([{
-                "Awarded amount in kW": awarded_amount,
-                "Provided power in kW": provided_power,
-                "Payoff in ct": payoff,
-                "Balance after transaction in ct": self.balance
+                AWARDED_AMOUNT: awarded_amount,
+                PROVIDED_POWER: provided_power,
+                PAYOFF: payoff,
+                BALANCE: balance
             }])
         ], ignore_index=True)
 
     def get_balance(self):
-        return self.balance
+        return self.transactions[BALANCE].iloc[-1]
 
     def get_transactions(self):
         return self.transactions
--- hackathon_backend/accounting/accounter.py
+++ hackathon_backend/accounting/accounter.py
@@ -1,6 +1,9 @@
 from hackathon_backend.market.auction import AuctionResult
 import pandas as pd
 
+AMOUNT = "Amount in kW"
+PRICE = "Price in ct per kW"
+
 class ElectricityAskAuctionAccounter:
     def __init__(self, auction_result: AuctionResult):
         self.result = auction_result
@@ -13,37 +16,37 @@ class ElectricityAskAuctionAccounter:
             agent = awarded_order.agent
             if agent not in agent_dataframes:
                 agent_dataframes[agent] = pd.DataFrame(
-                    columns=["Amount in kW", "Price in ct per kW"])
+                    columns=[AMOUNT, PRICE])
 
             agent_dataframes[agent] = pd.concat([
                 agent_dataframes[agent], 
                 pd.DataFrame([{
-                    "Amount in kW": awarded_order.awarded_amount_kw,
-                    "Price in ct per kW": awarded_order.price_ct
+                    AMOUNT: awarded_order.awarded_amount_kw,
+                    PRICE: awarded_order.price_ct
                 }])
             ], ignore_index=True)
 
         for agent in agent_dataframes:
             agent_dataframes[agent] = agent_dataframes[agent].sort_values(
-                by="Price in ct per kW", ascending=ascending).reset_index(drop=True)
+                by=PRICE, ascending=ascending).reset_index(drop=True)
         return agent_dataframes
     
     def return_awarded_sum(self, agent):
         if self.result is None or agent not in self.awarded_orders:
             return 0
         
-        return self.awarded_orders[agent]["Amount in kW"].sum()
+        return self.awarded_orders[agent][AMOUNT].sum()
     
     def calculate_payoff(self, agent, total_provided_amount):
         if self.result is None or agent not in self.awarded_orders:
             return 0
         
-        total_awarded_amount = sum(self.awarded_orders[agent]["Amount in kW"])
+        total_awarded_amount = sum(self.awarded_orders[agent][AMOUNT])
 
         if total_provided_amount >= total_awarded_amount:
             return (
-                self.awarded_orders[agent]["Price in ct per kW"] *
-                self.awarded_orders[agent]["Amount in kW"]
+                self.awarded_orders[agent][PRICE] *
+                self.awarded_orders[agent][AMOUNT]
             ).sum()
         
         elif total_provided_amount <= 0:
@@ -52,12 +55,12 @@ class ElectricityAskAuctionAccounter:
             amount_added_up = 0
             payoff = 0
             for index, row in self.awarded_orders[agent].iterrows():
-                if amount_added_up + row["Amount in kW"] <= total_provided_amount:
-                    payoff += row["Price in ct per kW"] * row["Amount in kW"]
-                    amount_added_up += row["Amount in kW"]
+                if amount_added_up + row[AMOUNT] <= total_provided_amount:
+                    payoff += row[PRICE] * row[AMOUNT]
+                    amount_added_up += row[AMOUNT]
                 else:
                     remaining_amount = total_provided_amount - amount_added_up
-                    payoff += row["Price in ct per kW"] * remaining_amount
+                    payoff += row[PRICE] * remaining_amount
                     break
 
             return payoff
