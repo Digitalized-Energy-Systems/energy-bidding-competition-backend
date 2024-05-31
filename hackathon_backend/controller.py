47d8249c71347acc1580cdbb253796bbec90b6ce
Paul Hendrik Tiemann <paul.hendrik.tiemann@uni-oldenburg.de>
2024-05-31 09:38:09 +0200

    Improve stepping order or units and market for more realistic agent experience.

--- hackathon_backend/controller.py
+++ hackathon_backend/controller.py
@@ -17,6 +17,7 @@ from hackathon_backend.accounting.account import Account
 
 
 DEFAULT_CONFIG_FILE = "config.json"
+SECONDS_PER_STEP = 900
 
 
 class ControlException(Exception):
@@ -71,8 +72,11 @@ class Controller:
     def __init__(self, config: Config = None):
         self.market = Market()
         self.unit_pool = UnitPool()
-        self.current_task = asyncio.Future()
-        self.current_task.set_result(None)
+        self.registration_open = True
+        self.current_market_task = asyncio.Future()
+        self.current_market_task.set_result(None)
+        self.current_unit_task = asyncio.Future()
+        self.current_unit_task.set_result(None)
         self.registered = set()
         self.config = load_default_config() if config is None else config
         self.step = 0
@@ -87,25 +91,24 @@ class Controller:
         while True:
             while self.config.pause:
                 asyncio.sleep(1)
+            self.registration_open = False
 
+            self.current_market_task = asyncio.create_task(self.loop_market(self.step))
             await asyncio.sleep(self.config.rt_step_duration_s)
-            self.current_task = asyncio.create_task(self.loop(self.step))
+            self.current_unit_task = asyncio.create_task(self.loop_units(self.step))
+            print(f"Step finished...{self.step}")
             self.step += 1
 
-    async def loop(self, time_step):
-        # TODO step 15 minutes ahead
-        current_time = time.time()
-        current_time = time_step * 900
-
-        self.current_time = current_time  # only for testing
-
-        self.step_market(current_time=current_time)
-        self.step_units(current_time=current_time)
-        print(f"Step finished...{current_time}")
+    async def loop_market(self, time_step):
+        self.step_market(current_time=time_step * SECONDS_PER_STEP)
+        
+    async def loop_units(self, time_step):
+        self.step_units(current_time=time_step * SECONDS_PER_STEP)
 
     async def get_current_time(self):  # only for testing
-        await self.check_step_done()
-        return self.current_time
+        await self.check_market_step_done()
+        await self.check_unit_step_done()
+        return self.step * SECONDS_PER_STEP
 
     def step_market(self, current_time):
         """Step market to next time interval.
@@ -158,29 +161,35 @@ class Controller:
             )
             print(f"Payoff for actor {actor_id}... {payoff}")
 
-    async def check_step_done(self):
-        if not self.current_task.done():
-            await self.current_task
+    async def check_market_step_done(self):
+        if not self.current_market_task.done():
+            await self.current_market_task
+
+    async def check_unit_step_done(self):
+        if not self.current_unit_task.done():
+            await self.current_unit_task
 
     async def register_actor(self, participant_id: str) -> List[UnitInformation]:
-        await self.check_step_done()
-        print(f"Registering actor {participant_id}...")
+        if self.registration_open:
+            print(f"Registering actor {participant_id}...")
 
-        if participant_id in self.config.participants:
-            if participant_id in self.registered:
-                raise ControlException(400, "The participant is already registered!")
-            self.registered.add(participant_id)
-            print(f"Registered actor {participant_id}...")
+            if participant_id in self.config.participants:
+                if participant_id in self.registered:
+                    raise ControlException(400, "The participant is already registered!")
+                self.registered.add(participant_id)
+                print(f"Registered actor {participant_id}...")
+            else:
+                raise ControlException(403, "The requester is unknown!")
+
+            actor_id, root_unit = allocate_default_actor_units()
+            self.unit_pool.insert_actor_root(actor_id, root_unit)
+            self.actor_accounts[actor_id] = Account()
+            return actor_id, self.unit_pool.read_units(actor_id)
         else:
-            raise ControlException(403, "The requester is unknown!")
-
-        actor_id, root_unit = allocate_default_actor_units()
-        self.unit_pool.insert_actor_root(actor_id, root_unit)
-        self.actor_accounts[actor_id] = Account()
-        return actor_id, self.unit_pool.read_units(actor_id)
+            raise ControlException(405, "Registration is closed!")
 
     async def read_units(self, actor_id) -> List[UnitInformation]:
-        await self.check_step_done()
+        await self.check_unit_step_done()
 
         if not self.unit_pool.has_actor(actor_id):
             raise ControlException(404, "The actor id does not exist!")
@@ -188,7 +197,7 @@ class Controller:
 
     async def return_open_auction_params(self):
         """Return open auction params to enable actors to place orders."""
-        await self.check_step_done()
+        await self.check_market_step_done()
         return [auction["params"] for auction in self.market.get_open_auctions()]
 
     async def receive_order(self, actor_id, amount_kw, price_ct, supply_time):
@@ -197,7 +206,7 @@ class Controller:
         :param order: Order object
         :param supply_time: Supply time of the auction (key to select auction)
         """
-        await self.check_step_done()
+        await self.check_market_step_done()
 
         if self.market.receive_order(
             amount_kw=amount_kw,
@@ -214,7 +223,7 @@ class Controller:
         """Return awarded orders for actor.
         :param actor_id: Actor identifier
         """
-        await self.check_step_done()
+        await self.check_market_step_done()
 
         current_results = self.market.get_current_auction_results()
         relevant_results = {}
@@ -236,7 +245,7 @@ class Controller:
         """
         Returns current auction results based on product type and supply time
         """
-        await self.check_step_done()
+        await self.check_market_step_done()
         return self.market.get_current_auction_results()
 
     def reset(self):
